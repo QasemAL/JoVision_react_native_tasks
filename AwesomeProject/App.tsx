417131e2dcd2e8ef2e186152f80510541659fc0c
QasemAL <129743072+QasemAL@users.noreply.github.com>
2024-05-23 10:40:28 +0300

    task15

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {View , Text} from 'react-native';
+// This where everything will be rendered to the screen
+
+export default class App extends Component {
+ 
+
+
+
+  render() {
+    return (
+
+  <View><Text>This is working</Text></View>
+
+ 
+ 
+  );
+}
+}
