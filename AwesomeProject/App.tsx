f9df3d89441484cc6e755f7324bfcb34e851a7b0
QasemAL <129743072+QasemAL@users.noreply.github.com>
2024-07-21 02:16:24 +0300

    Task38

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -22,6 +22,7 @@ import Task34 from './Tasks/Task34';
 import Task35 from './Tasks/Task35';
 import Task36 from './Tasks/Task36';
 import Task37 from './Tasks/Task37';
+import Task38 from './Tasks/Task38';
 // This where everything will be rendered to the screen
 
 export default class App extends Component {
@@ -52,7 +53,8 @@ export default class App extends Component {
         {/*<Task34></Task34>*/}
         {/*<Task35></Task35>*/}
         {/*<Task36></Task36>*/}
-           <Task37></Task37>
+        {/*<Task37></Task37>*/}
+           <Task38></Task38>
         </View>
     
      
